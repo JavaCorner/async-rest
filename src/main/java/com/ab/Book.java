039e95cf0c9602cb18788adb504c37fd5cf6dd93
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-04-23 14:56:46 +0530

    Removed the server side code dependency from test layer

--- src/main/java/com/ab/Book.java
+++ src/main/java/com/ab/Book.java
@@ -1,22 +1,26 @@
 package com.ab;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.*;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Arpit Bhardwaj
  */
 @JsonPropertyOrder({"id"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Book {
 
     private String title;
     private String author;
-    private String isbn;
+    //private String isbn;
     private Date published;
     private String id;
+
+    private Map<String,Object> extras = new HashMap<>();
 
     public String getId() {
         return id;
@@ -42,13 +46,13 @@ public class Book {
         this.author = author;
     }
 
-    public String getIsbn() {
+    /*public String getIsbn() {
         return isbn;
     }
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
-    }
+    }*/
 
     public Date getPublished() {
         return published;
@@ -57,4 +61,15 @@ public class Book {
     public void setPublished(Date published) {
         this.published = published;
     }
+
+    @JsonAnyGetter
+    public Map<String, Object> getExtras() {
+        return extras;
+    }
+
+    @JsonAnySetter
+    public void setExtras(String key, Object value) {
+        this.extras.put(key,value);
+    }
+
 }
--- src/test/java/com/ab/BookResourceTest.java
+++ src/test/java/com/ab/BookResourceTest.java
@@ -13,8 +13,12 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -35,39 +39,73 @@ public class BookResourceTest extends JerseyTest {
 
     @Before
     public void setUpBooks(){
-        book_id1 = addBook("author1","title1",new Date(), "isbn1").readEntity(Book.class).getId();
-        book_id2 = addBook("author2","title2",new Date(), "isbn2").readEntity(Book.class).getId();
+        /*book_id1 = addBook("author1","title1",new Date(), "isbn1").readEntity(Book.class).getId();
+        book_id2 = addBook("author2","title2",new Date(), "isbn2").readEntity(Book.class).getId();*/
+        book_id1 = (String) deserializeResponseToMap(addBook("author1","title1",new Date(), "isbn1")).get("id");
+        book_id2 = (String) deserializeResponseToMap(addBook("author2","title2",new Date(), "isbn2")).get("id");
     }
     @Test
     public void testAddBook(){
+        Date thisDate = new Date();
 
-        Response response = addBook("author","title",new Date(), "isbn");
+        Response response = addBook("author","title",thisDate, "isbn");
         assertEquals(200, response.getStatus());
-        Book readEntity = response.readEntity(Book.class);
+
+        /*Book readEntity = response.readEntity(Book.class);
         assertNotNull(readEntity.getId());
         assertEquals("title",readEntity.getTitle());
         assertEquals("author",readEntity.getAuthor());
+        assertEquals(thisDate,readEntity.getPublished());
+        assertEquals("isbn",readEntity.getIsbn());*/
 
+        Map<String,Object> readEntity = deserializeResponseToMap(response);
+        assertNotNull(readEntity.get("id"));
+        assertEquals("title",readEntity.get("title"));
+        assertEquals("author",readEntity.get("author"));
+        /*SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'hh:mm:ss.SSSZ");
+        try {
+            assertEquals(thisDate,dateFormat.parse((String) readEntity.get("published")));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }*/
+        assertEquals("isbn",readEntity.get("isbn"));
     }
-    protected Response addBook(String author, String title, Date published, String isbn){
-        Book book = new Book();
+    protected Response addBook(String author, String title, Date published, String isbn, String... extras){
+        /*Book book = new Book();
         book.setTitle(title);
         book.setAuthor(author);
         book.setPublished(published);
-        book.setIsbn(isbn);
+        book.setIsbn(isbn);*/
 
-        Entity<Book> bookEntity = Entity.entity(book, MediaType.APPLICATION_JSON_TYPE);
+        Map<String,Object> map = new HashMap<>();
+        map.put("author",author);
+        map.put("title",title);
+        map.put("published",published);
+        map.put("isbn", isbn);
+
+        if (extras != null){
+            int count = 1;
+            for (String extra:
+                 extras) {
+                map.put("extra" + count++,extra);
+            }
+        }
+        //Entity<Book> bookEntity = Entity.entity(book, MediaType.APPLICATION_JSON_TYPE);
+        Entity<Map<String,Object>> bookEntity = Entity.entity(map, MediaType.APPLICATION_JSON_TYPE);
         return target("books").request().post(bookEntity);
     }
     @Test
     public void testGetBook(){
-        Book response = target("books").path(book_id1).request().get(Book.class);
+        //Book response = target("books").path(book_id1).request().get(Book.class);
+        Map<String,Object> response = deserializeResponseToMap(target("books").path(book_id1).request().get());
         assertNotNull(response);
     }
 
     @Test
     public void testGetBooks(){
-        Collection<Book> response = target("books").request().get(new GenericType<Collection<Book>>() {});
+        //Collection<Book> response = target("books").request().get(new GenericType<Collection<Book>>() {});
+        Collection<Map<String,Object>> response = target("books").request()
+                .get(new GenericType<Collection<Map<String,Object>>>() {});
         assertEquals(2,response.size());
     }
 
@@ -78,4 +116,18 @@ public class BookResourceTest extends JerseyTest {
         assertEquals(response1.getPublished().getTime(),response2.getPublished().getTime());
     }*/
 
+    @Test
+    public void testAddExtraField(){
+        Response response = addBook("author","title",new Date(), "isbn","extra1");
+        assertEquals(200, response.getStatus());
+        Map<String,Object> readEntity = deserializeResponseToMap(response);
+        assertNotNull(readEntity.get("id"));
+        assertEquals("title",readEntity.get("title"));
+        assertEquals("author",readEntity.get("author"));
+        assertEquals("extra1",readEntity.get("extra1"));
+    }
+
+    public Map<String,Object> deserializeResponseToMap(Response response){
+        return response.readEntity(new GenericType<Map<String, Object>>() {});
+    }
 }
